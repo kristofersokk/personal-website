3aae5afeeca7a74cbde1f2d43b3ca10d96508725
Kristofer Sokk <krsokk@ttu.ee>
2022-08-07 17:54:54 +0300

    Remove gzip compression, refactor hsvtorgb function

--- src/components/MainCanvas.tsx
+++ src/components/MainCanvas.tsx
@@ -136,39 +136,36 @@ const MainCanvas: FC = () => {
  * h, s, v
  */
 function HSVtoRGB(h: number, s: number, v: number) {
-	let r = 0,
-		g = 0,
-		b = 0;
-	let i, f, p, q, t;
-	i = Math.floor(h * 6);
-	f = h * 6 - i;
-	p = v * (1 - s);
-	q = v * (1 - f * s);
-	t = v * (1 - (1 - f) * s);
+	let rgb = [0, 0, 0];
+	const i = Math.floor(h * 6);
+	const f = h * 6 - i;
+	const p = v * (1 - s);
+	const q = v * (1 - f * s);
+	const t = v * (1 - (1 - f) * s);
 	switch (i % 6) {
 		case 0:
-			(r = v), (g = t), (b = p);
+			rgb = [v, t, p];
 			break;
 		case 1:
-			(r = q), (g = v), (b = p);
+			rgb = [q, v, p];
 			break;
 		case 2:
-			(r = p), (g = v), (b = t);
+			rgb = [p, v, t];
 			break;
 		case 3:
-			(r = p), (g = q), (b = v);
+			rgb = [p, q, v];
 			break;
 		case 4:
-			(r = t), (g = p), (b = v);
+			rgb = [t, p, v];
 			break;
 		case 5:
-			(r = v), (g = p), (b = q);
+			rgb = [v, p, q];
 			break;
 	}
 	return {
-		r: Math.round(r * 255),
-		g: Math.round(g * 255),
-		b: Math.round(b * 255),
+		r: Math.round(rgb[0] * 255),
+		g: Math.round(rgb[1] * 255),
+		b: Math.round(rgb[2] * 255),
 	};
 }
 
